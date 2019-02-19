63795a85a05e4810714b9df5b5aa87681875bf39
Christopher Mühl <christopher@dieschittigs.de>
2019-02-19 10:59:14 +0100

    Create a usable TypeScript setup

--- cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const Runtime = require('./dist').default;
-
-const configuration = require(process.cwd() + '/blurple')
-
-var runtime = new Runtime();
-runtime.start(configuration);
